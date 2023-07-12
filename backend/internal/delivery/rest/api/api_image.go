303227197bd9959b5bbb683dda1420731c247927
axel <axel.rl.404@gmail.com>
2023-07-12 13:55:19 +0700

    deps: chi v5

--- backend/internal/delivery/rest/api/api_image.go
+++ backend/internal/delivery/rest/api/api_image.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"github.com/rl404/image-randomizer/internal/errors"
 	"github.com/rl404/image-randomizer/internal/service"
 	"github.com/rl404/image-randomizer/internal/utils"
